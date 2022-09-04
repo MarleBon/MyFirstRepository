6f5fb8af01a0d723de335eabe6ecf16100a54e62
MarleBon <112806331+MarleBon@users.noreply.github.com>
2022-09-04 12:25:12 +0100

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.read_excel(r"C:\Users\marle\OneDrive\Desktop\data2.xlsx")
+
+
+# In[6]:
+
+
+pd.read_csv(r"C:\Users\marle\OneDrive\Desktop\data3.csv")
+
+
+# In[14]:
+
+
+pd.read_csv(r"C:\Users\marle\OneDrive\Desktop\data4.tsv")
+
+
+# In[ ]:
+
+
+
+
